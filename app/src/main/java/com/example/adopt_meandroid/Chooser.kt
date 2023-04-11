9801c98923d14794a854d661d4326b7825fb271a
Victor <ramirez@calimasolutions.com>
2023-04-11 13:27:55 +0100

    Pick up the photo with use url

--- app/src/main/java/com/example/adopt_meandroid/Chooser.kt
+++ app/src/main/java/com/example/adopt_meandroid/Chooser.kt
@@ -1,12 +1,21 @@
 package com.example.adopt_meandroid
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
 
 class Chooser : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chooser)
-
+        var imageView = findViewById<ImageView>(R.id.PetPhoto)
+        Picasso.get()
+            .load("https://www.catalunyaplants.com/wp-content/uploads/2015/01/golden-retriever.jpg")
+            .resize(200, 200)
+            .centerCrop()
+            .into(imageView)
     }
 }
